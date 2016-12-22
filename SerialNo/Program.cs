bf972f8b740c0c8cb7b547e594edca49b6136b6e
cwi-saito <saito@contentsworks.co.jp>
2016-12-22 17:14:01 +0900

    いろいろ

---
+++ SerialNo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SerialNo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var copyFrom = @"C:\Users\saito\Pictures\数\default.jpg";
+            var copyTo = @"C:\Users\saito\Pictures\数\copy.jpg";
+            var root = @"C:\Users\saito\Pictures\数\";
+            File.Copy(copyFrom, copyTo, true);
+            for (var i = 81; i < 150;i++ )
+                File.Copy(copyTo, root + "0" + i.ToString() + ".jpg", true);
+        }
+    }
+}
