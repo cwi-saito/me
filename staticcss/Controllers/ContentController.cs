37915ff71c9e45e1aeecf178e332a4f4f4aba65b
cwi-saito <saito@contentsworks.co.jp>
2017-02-20 18:11:32 +0900

    cssをレスポンスするプロジェクトを作成

---
+++ staticcss/Controllers/ContentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.Web.UI;
+
+namespace staticcss.Controllers
+{
+    public class ContentController : Controller
+    {
+        /// <summary>
+        /// ~content/index/{file}
+        /// content/{file}.cssのレスポンスを変換
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public ActionResult Index(string file)
+        {
+            var path = Server.MapPath("~/Content/" + file + ".css");
+
+            // 空白除去
+            var content = string.Join("", System.IO.File.ReadAllLines(path).Select(p => p.Trim()));
+
+            // css形式のMINEタイプでレスポンスを返す
+            return Content(content, "text/css");            
+        }
+    }
+}
